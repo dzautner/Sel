d7854f5da3a1f63ffd922e5c688df3ae4720a543
Daniel Zautner <dzautner@gmail.com>
2017-04-24 15:41:35 +0300

    build

--- out/compiler.js
+++ out/compiler.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.toJS = undefined;
+exports.toJS = exports.toChurchNotation = exports.toPlainJavascript = undefined;
 
 var _errors = require('./errors');
 
@@ -11,39 +11,86 @@ const firstChildType = node => node.children[0].body.type;
 
 const isApplication = node => firstChildType(node) !== 'VAR_DEC' && firstChildType(node) !== 'LAMBDA_DEC';
 
-const toJS = exports.toJS = node => {
-  const currentNodeType = node.body.type;
-  let buffer = '';
-  switch (currentNodeType) {
+const toPlainJavascript = exports.toPlainJavascript = node => {
+  switch (node.body.type) {
     case 'PROGRAM':
-      buffer = node.children.map(toJS).join(';\n') + ';';break;
+      return node.children.map(toJS).join(';\n') + ';';
     case 'VAR_DEC':
-      buffer = `const ${node.body.name} = `;
-      break;
+      return `const ${node.body.name} = `;
     case 'LAMBDA_DEC':
-      buffer = `(${node.body.input} => ${node.children.map(toJS).join('')})`;
-      break;
+      return `(${node.body.input} => ${node.children.map(toPlainJavascript).join('')})`;
     case 'ATOM':
-      buffer = node.body.name;
-      break;
+      return node.body.name;
     case 'LIST':
       if (node.children.length === 0) {
         (0, _errors.throwEmptyListError)();
-        break;
       }
       if (isApplication(node)) {
         if (node.children.length < 2) {
           (0, _errors.throwEmptyApplicationError)();
-          break;
         }
         const namedLambdaNode = node.children[0];
         const lambdaArgumentNode = node.children[1];
-        buffer = `${toJS(namedLambdaNode)}(${toJS(lambdaArgumentNode)})`;
-      } else {
-        buffer = node.children.map(toJS).join('');
+        return `${toPlainJavascript(namedLambdaNode)}(${toPlainJavascript(lambdaArgumentNode)})`;
       }
-      break;
+      return node.children.map(toPlainJavascript).join('');
   }
-
-  return buffer;
 };
+
+// const varMap = {};
+// // export const toJS = (node: ASTNode): ?string => {
+// //   switch (node.body.type) {
+// //   case 'PROGRAM':
+// //     return node.children.map(toJS).join('');
+// //   case 'VAR_DEC':
+// //     varMap[node.body.name] = node.children.map(toJS).join('');
+// //     return
+// //   case 'LAMBDA_DEC':
+// //     return `(${node.body.input} => ${node.children.map(toJS).join('')})`;
+// //   case 'ATOM':
+// //     return varMap[node.body.name] || node.body.name;
+// //   case 'LIST':
+// //     if (node.children.length === 0) {
+// //       throwEmptyListError();
+// //     }
+// //     if (isApplication(node)) {
+// //       if (node.children.length < 2) {
+// //         throwEmptyApplicationError();
+// //       }
+// //       const namedLambdaNode = node.children[0];
+// //       const lambdaArgumentNode = node.children[1];
+// //       return `${toJS(namedLambdaNode)}(${toJS(lambdaArgumentNode)})`;
+// //     }
+// //     return node.children.map(toJS).join('');
+// //   }
+// // };
+
+const churchVarMap = {};
+const toChurchNotation = exports.toChurchNotation = node => {
+  switch (node.body.type) {
+    case 'PROGRAM':
+      return node.children.map(toChurchNotation).join('');
+    case 'VAR_DEC':
+      churchVarMap[node.body.name] = node.children.map(toChurchNotation).join('');
+      return;
+    case 'LAMBDA_DEC':
+      return `(lambda ${node.body.input}.${node.children.map(toChurchNotation).join('')})`;
+    case 'ATOM':
+      return churchVarMap[node.body.name] || node.body.name;
+    case 'LIST':
+      if (node.children.length === 0) {
+        (0, _errors.throwEmptyListError)();
+      }
+      if (isApplication(node)) {
+        if (node.children.length < 2) {
+          (0, _errors.throwEmptyApplicationError)();
+        }
+        const namedLambdaNode = node.children[0];
+        const lambdaArgumentNode = node.children[1];
+        return `(${toChurchNotation(namedLambdaNode)} (${toChurchNotation(lambdaArgumentNode)}))`;
+      }
+      return node.children.map(toChurchNotation).join('');
+  }
+};
+
+const toJS = exports.toJS = toChurchNotation;
--- out/parser.js
+++ out/parser.js
@@ -26,6 +26,12 @@ exports.default = tokens => {
     const nextToken = tokens[tokenPointer + 1];
     switch (token.type) {
       case 'OPEN_PARA':
+        if (nextToken.type === 'VAR_DEC') {
+          addAndMoveToNode(_extends({}, tokens[++tokenPointer], {
+            name: tokens[++tokenPointer].name
+          }));
+          break;
+        }
         if (nextToken.type === 'LAMBDA_DEC') {
           addAndMoveToNode(_extends({}, tokens[++tokenPointer], {
             input: tokens[++tokenPointer].name
@@ -37,16 +43,6 @@ exports.default = tokens => {
         }
       case 'CLOSE_PARA':
         closeCurrentNode();break;
-      case 'VAR_DEC':
-        addToCurrentNode(_extends({}, token, {
-          name: tokens[++tokenPointer].name
-        }));
-        break;
-      case 'LAMBDA_DEC':
-        addAndMoveToNode(_extends({}, token, {
-          input: tokens[++tokenPointer].name
-        }));
-        break;
       default:
         addToCurrentNode(token);
     }
