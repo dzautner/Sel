8db444f6cac21e462f5c0eeaf9e3d550c84a547a
Daniel Zautner <dzautner@gmail.com>
2017-04-24 16:17:30 +0300

    add coverage

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Sel
+[![Coverage Status](https://coveralls.io/repos/dzautner/Sel/badge.svg?branch=master)](https://coveralls.io/r/dzautner/Sel?branch=master)
--- out/compiler.js
+++ out/compiler.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.toJS = exports.toChurchNotation = exports.toPlainJavascript = undefined;
+exports.toChurchNotation = exports.toPointFreeJS = exports.toJS = undefined;
 
 var _errors = require('./errors');
 
@@ -11,14 +11,14 @@ const firstChildType = node => node.children[0].body.type;
 
 const isApplication = node => firstChildType(node) !== 'VAR_DEC' && firstChildType(node) !== 'LAMBDA_DEC';
 
-const toPlainJavascript = exports.toPlainJavascript = node => {
+const toJS = exports.toJS = node => {
   switch (node.body.type) {
     case 'PROGRAM':
       return node.children.map(toJS).join(';\n') + ';';
     case 'VAR_DEC':
-      return `const ${node.body.name} = `;
+      return `const ${node.body.name} = ${node.children.map(toJS).join('')}`;
     case 'LAMBDA_DEC':
-      return `(${node.body.input} => ${node.children.map(toPlainJavascript).join('')})`;
+      return `(${node.body.input} => ${node.children.map(toJS).join('')})`;
     case 'ATOM':
       return node.body.name;
     case 'LIST':
@@ -31,39 +31,39 @@ const toPlainJavascript = exports.toPlainJavascript = node => {
         }
         const namedLambdaNode = node.children[0];
         const lambdaArgumentNode = node.children[1];
-        return `${toPlainJavascript(namedLambdaNode)}(${toPlainJavascript(lambdaArgumentNode)})`;
+        return `${toJS(namedLambdaNode)}(${toJS(lambdaArgumentNode)})`;
       }
-      return node.children.map(toPlainJavascript).join('');
+      return node.children.map(toJS).join('');
   }
 };
 
-// const varMap = {};
-// // export const toJS = (node: ASTNode): ?string => {
-// //   switch (node.body.type) {
-// //   case 'PROGRAM':
-// //     return node.children.map(toJS).join('');
-// //   case 'VAR_DEC':
-// //     varMap[node.body.name] = node.children.map(toJS).join('');
-// //     return
-// //   case 'LAMBDA_DEC':
-// //     return `(${node.body.input} => ${node.children.map(toJS).join('')})`;
-// //   case 'ATOM':
-// //     return varMap[node.body.name] || node.body.name;
-// //   case 'LIST':
-// //     if (node.children.length === 0) {
-// //       throwEmptyListError();
-// //     }
-// //     if (isApplication(node)) {
-// //       if (node.children.length < 2) {
-// //         throwEmptyApplicationError();
-// //       }
-// //       const namedLambdaNode = node.children[0];
-// //       const lambdaArgumentNode = node.children[1];
-// //       return `${toJS(namedLambdaNode)}(${toJS(lambdaArgumentNode)})`;
-// //     }
-// //     return node.children.map(toJS).join('');
-// //   }
-// // };
+const varMap = {};
+const toPointFreeJS = exports.toPointFreeJS = node => {
+  switch (node.body.type) {
+    case 'PROGRAM':
+      return node.children.map(toPointFreeJS).join('');
+    case 'VAR_DEC':
+      varMap[node.body.name] = node.children.map(toPointFreeJS).join('');
+      return;
+    case 'LAMBDA_DEC':
+      return `(${node.body.input} => ${node.children.map(toPointFreeJS).join('')})`;
+    case 'ATOM':
+      return varMap[node.body.name] || node.body.name;
+    case 'LIST':
+      if (node.children.length === 0) {
+        (0, _errors.throwEmptyListError)();
+      }
+      if (isApplication(node)) {
+        if (node.children.length < 2) {
+          (0, _errors.throwEmptyApplicationError)();
+        }
+        const namedLambdaNode = node.children[0];
+        const lambdaArgumentNode = node.children[1];
+        return `${toPointFreeJS(namedLambdaNode)}(${toPointFreeJS(lambdaArgumentNode)})`;
+      }
+      return node.children.map(toPointFreeJS).join('');
+  }
+};
 
 const churchVarMap = {};
 const toChurchNotation = exports.toChurchNotation = node => {
@@ -92,5 +92,3 @@ const toChurchNotation = exports.toChurchNotation = node => {
       return node.children.map(toChurchNotation).join('');
   }
 };
-
-const toJS = exports.toJS = toChurchNotation;
